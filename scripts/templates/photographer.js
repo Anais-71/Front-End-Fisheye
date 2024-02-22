4dfd26008dda4dbf9c5b19bdeb19b0de6316639b
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-02-22 16:35:18 +0100

    feat: data mapping

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,35 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        //photographer card creation
+        const article = document.createElement('article');
+            //image recovery
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+            //name recovery
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+            //location recovery
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ", " + country;
+            //tagline recovery
+        const h4 = document.createElement('h4');
+        h4.textContent = tagline;
+            //price
+        const p = document.createElement('p');
+        p.textContent = price + "€/jour";
+
+        //DOM elements creation
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
